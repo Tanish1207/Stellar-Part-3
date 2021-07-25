8c35f3a58e0578a26814e3a695dabd101186701a
Tanish1207 <71355271+Tanish1207@users.noreply.github.com>
2021-07-25 11:01:44 -0400

    Add files via upload

---
+++ DailyPicScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View, TouchableOpacity } from 'react-native'
+
+
+export default class DailyPicScreen extends React.Component{
+    render(){
+        return(
+            <View style={{alignItems:'center',justifyContent:'center', flex:1}}>
+<Text>Daily Pic Screen</Text>
+            </View>
+        )
+    }
+}
---
+++ SpaceCraftsScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View, TouchableOpacity } from 'react-native'
+
+
+export default class SpaceCraftsScreen extends React.Component{
+    render(){
+        return(
+            <View style={{alignItems:'center',justifyContent:'center', flex:1}}>
+<Text>Space Craft Screen</Text>
+            </View>
+        )
+    }
+}
